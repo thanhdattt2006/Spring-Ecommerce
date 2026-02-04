609c3d5dc035ccdfe2ced052c821216599b566d2
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-02-04 22:53:15 +0700

    chá»ore (application.á»properties): add new package  and Product.java in package model nd cÃonfig application.properties to launch h2 dÃatabase

---
+++ backend/src/main/java/com/dave/backend/model/Product.java
@@ -0,0 +1,36 @@
+package com.dave.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity // Đánh dấu đây là bảng trong DB
+@Data // Lombok: Tự tạo Getter, Setter, toString...
+@AllArgsConstructor // Lombok: Constructor full tham số
+@NoArgsConstructor // Lombok: Constructor rỗng (Bắt buộc cho JPA)
+public class Product {
+    @Id
+    private int id;
+
+    private String name;
+    private String description;
+    private String brand;
+
+    // Giá tiền nên dùng BigDecimal để tránh lỗi làm tròn số
+    private BigDecimal price;
+
+    private String category;
+
+    // Ngày tháng dùng java.util.Date
+    private Date releaseDate;
+
+    private boolean productAvailable;
+    private int stockQuantity; // Số lượng tồn kho
+
+    // Sau này sẽ thêm trường ảnh (image) ở đây
+}
